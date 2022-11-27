2abd5d5f97adafd074cf87bdd1da34e391a2f7da
WGMI <imo.muganda21@gmail.com>
2022-11-27 12:55:41 +0300

    Fetching and displaying search results

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -14,56 +14,8 @@
 			</div>
 
 			<div class="product-list" data-aos="fade-up">
-				<div class="row">
-
-					<div class="col-md-3">
-						<figure class="product-style">
-							<img src="images/product-item1.jpg" alt="Books" class="product-item">
-								<button type="button" class="add-to-cart" data-product-tile="add-to-cart">Add to Cart</button>
-							<figcaption>
-								<h3>Simple way of piece life</h3>
-								<p>Armor Ramsey</p>
-								<div class="item-price">$ 40.00</div>
-							</figcaption>
-						</figure>
-					</div>
-				
-					<div class="col-md-3">
-						<figure class="product-style">
-							<img src="images/product-item2.jpg" alt="Books" class="product-item">
-								<button type="button" class="add-to-cart" data-product-tile="add-to-cart">Add to Cart</button>
-							<figcaption>
-								<h3>Great travel at desert</h3>
-								<p>Sanchit Howdy</p>
-								<div class="item-price">$ 38.00</div>
-							</figcaption>
-						</figure>
-					</div>
-
-					<div class="col-md-3">
-						<figure class="product-style">
-							<img src="images/product-item3.jpg" alt="Books" class="product-item">
-								<button type="button" class="add-to-cart" data-product-tile="add-to-cart">Add to Cart</button>
-							<figcaption>
-								<h3>The lady beauty Scarlett</h3>
-								<p>Arthur Doyle</p>
-								<div class="item-price">$ 45.00</div>
-							</figcaption>
-						</figure>
-					</div>
-									
-					<div class="col-md-3">
-						<figure class="product-style">
-							<img src="images/product-item4.jpg" alt="Books" class="product-item">
-								<button type="button" class="add-to-cart" data-product-tile="add-to-cart">Add to Cart</button>
-							<figcaption>
-								<h3>Once upon a time</h3>
-								<p>Klien Marry</p>
-								<div class="item-price">$ 35.00</div>
-							</figcaption>
-						</figure>
-					</div>
-
+				<div class="row" id="search-items">
+					
 			    </div><!--ft-books-slider-->				
 			</div><!--grid-->
 
@@ -91,14 +43,41 @@ window.addEventListener("load",(e) => {
 	queryLibrary(query)
 })
 
+const results = document.getElementById('search-items')
+
 const queryLibrary = (query) => {
 	axios.get(`http://openlibrary.org/search.json?q=${query}`)
 	.then((res) => {
-		console.log(res.data)
+		let data = res.data.docs
+		data.forEach(el => {
+			console.log(el)
+			let book = document.createElement('div')
+			book.setAttribute('class','col-md-2 book')
+			book.innerHTML = `
+			<figure class="product-style">
+				<a href=""><img src="images/product-item1.jpg" alt="Books" class="product-item"></a>
+				<figcaption>
+					<div class="titletext">
+						<h4>${shorten(el.title,18)}</h4>
+						<p>${shorten(el.author_name[0],16)}</p>
+						<span class="tooltiptext">${el.title} by ${el.author_name[0]}</span>
+					</div>
+				</figcaption>
+			</figure>
+			`
+			results.appendChild(book) 
+		});
 	})
 	.catch((err) => {
 		console.log(err)
 	})
+}
+
+const shorten = (text,length) => {
+	if(text.length > length){
+		return text.substring(0,length - 3) + '...'
+	}
+	return text
 }
 
 </script>
